c40a56c218766eee83470e3be1fe199332e77939
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2018-07-26 21:34:15 +0100

    Improved user places list design and some database bug fixes

--- app/src/main/java/com/cityguide/joaomjaneiro/cityguide/PointsOfInterest/Point_Activity.java
+++ app/src/main/java/com/cityguide/joaomjaneiro/cityguide/PointsOfInterest/Point_Activity.java
@@ -93,7 +93,7 @@ public class Point_Activity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     if(canAdd) {
-                        db.child("Users").child(user_id).child("locations").child(placeId).setValue(true);
+                        db.child("Users").child(user_id).child("locations").child(placeId).setValue(title);
                     }else {
                         Toast.makeText(Point_Activity.this, "That place is already saved", Toast.LENGTH_SHORT).show();
 
