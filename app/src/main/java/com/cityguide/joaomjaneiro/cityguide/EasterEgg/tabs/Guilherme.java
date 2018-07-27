710e3dec37c4aaf3a97456054606369627845c2e
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2018-07-27 15:46:20 +0100

    Added Guilherme Infos on EasterEgg

--- app/src/main/java/com/cityguide/joaomjaneiro/cityguide/EasterEgg/tabs/Guilherme.java
+++ app/src/main/java/com/cityguide/joaomjaneiro/cityguide/EasterEgg/tabs/Guilherme.java
@@ -1,10 +1,13 @@
 package com.cityguide.joaomjaneiro.cityguide.EasterEgg.tabs;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.cityguide.joaomjaneiro.cityguide.R;
 
@@ -13,6 +16,29 @@ public class Guilherme extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.guilherme_fragment, container, false);
+
+        ImageButton linkedIn = (ImageButton)rootView.findViewById(R.id.linkedIn2);
+        ImageButton gitHub = (ImageButton)rootView.findViewById(R.id.gitHub2);
+
+
+        linkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/joaomariajaneiro/"));
+                startActivity(browser);
+            }
+        });
+
+        gitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Guilherme-Viegas"));
+                startActivity(browser);
+            }
+        });
+
+
         return rootView;
+
     }
 }
